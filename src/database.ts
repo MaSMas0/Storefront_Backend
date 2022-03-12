0942b70b722ae97c1af1b2f7e2600e6f067e6e14
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-03-12 03:55:36 +0200

    add database configuration for db connection

---
+++ src/database.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config();
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_DB_TEST,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_PORT,
+  ENV
+} = process.env;
+
+const client = new Pool({
+  host: POSTGRES_HOST,
+  database: ENV === 'dev' ? POSTGRES_DB : POSTGRES_DB_TEST,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  port: parseInt(POSTGRES_PORT as string, 10)
+});
+export default client;
